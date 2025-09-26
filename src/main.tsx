3571a24a27175ed0757ea750559d1d014be1e45b
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-09-26 22:26:11 +0600

    Initial commit.
    1. Set up project.
    2. Add routing.

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+)
